2a1798b0a993dbfe082a968a1614736ba143a663
zenayorcloud <zenayorcloud@gmail.com>
2026-01-07 04:02:49 +0000

    Update telegram-hook.ts

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -13,7 +13,7 @@ const bot = new Telegraf(BOT_TOKEN);
 export async function handleStartCommand(ctx) {
   const COMMAND = "/start";
   const channelUrl = "t.me/gateProxy0";
-  const targetUrl = "t.me/+N1odfcmxpBRlMjE0";
+  const targetUrl = "t.me/+QS-WTkC1EKUxMTBk";
 
   // Welcome message with Markdown formatting
   const reply = `
